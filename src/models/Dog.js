681e60016770797fbd1710ca47f35f4cd75147af
unknown <bfrederick@BFREDERICK-D14.pictometry.com>
2015-10-15 14:50:27 -0400

    Inital MVC setup.  Added Dog model

---
+++ src/models/Dog.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+
+var DogModel = {};
+
+var DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    breed: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    age: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+
+    createdData: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+DogSchema.statics.findByName = function(name, callback){
+    var search = {
+        name: name
+    };
+
+    return DogModel.findOne(search, callback);
+};
+
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
